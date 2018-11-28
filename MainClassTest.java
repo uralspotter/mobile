7734c7457c26f6c7c264a9ab0984a82cd423a3e8
uralspotter <45425255+uralspotter@users.noreply.github.com>
2018-11-28 19:46:05 +0500

    Lecture_0_task_1

---
+++ MainClassTest.java
@@ -0,0 +1,11 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MainClassTest extends MainClass {
+
+    int number = this.getLocalNumber();
+    @Test
+    public void myTest() {
+        Assert.assertTrue("The proposed algorithm is equal to 14", number == 14);
+    }
+}
