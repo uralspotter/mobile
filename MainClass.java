aebef221ca917c548562e859837dd4e9eb547a72
Egor <45425255+uralspotter@users.noreply.github.com>
2018-11-29 10:02:03 +0500

    Lecture_0_task_2_2

--- MainClass.java
+++ MainClass.java
@@ -1,6 +1,10 @@
 public class MainClass {
 
-    
+    private int class_number = 20;
+
+    public int getClassNumber() {
+        return this.class_number;
+    }
 
     public int getLocalNumber() {
         return 14;
