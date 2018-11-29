5ffdaec837bf1898efbc6d454053c8b5d74f6fb1
Egor <45425255+uralspotter@users.noreply.github.com>
2018-11-29 09:59:41 +0500

    Lecture_0_task1_1

--- MainClass.java
+++ MainClass.java
@@ -1,11 +1,6 @@
 public class MainClass {
 
-    private int class_number = 20;
-    private string class_string = "Hello, world!";
-
-    public int getClassNumber() {
-        return this.class_number;
-    }
+    
 
     public int getLocalNumber() {
         return 14;
--- MainClassTest.java
+++ MainClassTest.java
@@ -4,15 +4,11 @@ import org.junit.Test;
 public class MainClassTest extends MainClass {
 
     int number = this.getLocalNumber();
-    int number2 = this.getClassNumber();
 
     @Test
     public void testGetLocalNumber() {
         Assert.assertTrue("The proposed algorithm is equal to 14", number == 14);
     }
 
-    @Test
-    public void testGetClassNumber() {
-        Assert.assertTrue("The number returned by less 45", number2 > 45);
-    }
+
 }
