131dae1423bdbfddeadb1d34cbc8f9410de7e797
Egor <45425255+uralspotter@users.noreply.github.com>
2019-01-02 06:25:17 -0800

    Add files via upload

--- javaAppiumAutomation/src/lib/ui/WelcomePageObject.java
+++ javaAppiumAutomation/src/lib/ui/WelcomePageObject.java
@@ -6,38 +6,38 @@ import org.openqa.selenium.By;
 public class WelcomePageObject extends MainPageObject {
 
     private static final String
-        STEP_LEARN_MORE_LINK = "Learn more about Wikipedia",
-        STEP_NEW_WAYS_TO_EXPLORE_LINK = "New ways to explore",
-        STEP_ADD_OR_EDIT_PREFERRD_LINK = "Add or edit preferred languages",
-        STEP_LEARN_MORE_ABOUT_DATA_COLLECTED_LINK = "Learn more about data collected",
-        NEXT_LINK = "Next",
-        GET_STARTED_LINK = "Get started";
+        STEP_LEARN_MORE_LINK = "id:Learn more about Wikipedia",
+        STEP_NEW_WAYS_TO_EXPLORE_LINK = "id:New ways to explore",
+        STEP_ADD_OR_EDIT_PREFERRD_LINK = "id:Add or edit preferred languages",
+        STEP_LEARN_MORE_ABOUT_DATA_COLLECTED_LINK = "id:Learn more about data collected",
+        NEXT_LINK = "id:Next",
+        GET_STARTED_LINK = "id:Get started";
 
     public WelcomePageObject(AppiumDriver driver) {
         super(driver);
     }
 
     public void waitForLearnMoreLink() {
-        this.waitForElementPresent(By.id(STEP_LEARN_MORE_LINK), "Cannot find 'Learn more about Wikipedia' link", 10);
+        this.waitForElementPresent(STEP_LEARN_MORE_LINK, "Cannot find 'Learn more about Wikipedia' link", 10);
     }
 
     public void waitForNewWayToExploreText() {
-        this.waitForElementPresent(By.id(STEP_NEW_WAYS_TO_EXPLORE_LINK), "Cannot find 'New ways to explore' link", 10);
+        this.waitForElementPresent(STEP_NEW_WAYS_TO_EXPLORE_LINK, "Cannot find 'New ways to explore' link", 10);
     }
 
     public void waitForAddOrEditPreferredText() {
-        this.waitForElementPresent(By.id(STEP_ADD_OR_EDIT_PREFERRD_LINK), "Cannot find 'Add or edit preferred launguages' link", 10);
+        this.waitForElementPresent(STEP_ADD_OR_EDIT_PREFERRD_LINK, "Cannot find 'Add or edit preferred languages' link", 10);
     }
 
     public void waitForLearnMoreAboutDataCollectedText() {
-        this.waitForElementPresent(By.id(STEP_LEARN_MORE_ABOUT_DATA_COLLECTED_LINK), "Cannot find 'Learn more about data collected' link", 10);
+        this.waitForElementPresent(STEP_LEARN_MORE_ABOUT_DATA_COLLECTED_LINK, "Cannot find 'Learn more about data collected' link", 10);
     }
 
     public void clickNextButton() {
-        this.waitForElementAndClick(By.id(NEXT_LINK), "Cannot find and click 'Next' button", 10);
+        this.waitForElementAndClick(NEXT_LINK, "Cannot find and click 'Next' button", 10);
     }
 
     public void clickGetStartedButton() {
-        this.waitForElementAndClick(By.id(GET_STARTED_LINK), "Cannot find and click 'Get started' button", 10);
+        this.waitForElementAndClick(GET_STARTED_LINK, "Cannot find and click 'Get started' button", 10);
     }
 }
